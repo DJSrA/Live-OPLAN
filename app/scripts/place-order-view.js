5ee996c1976e645aab54b5446c392b5fa3c20d3a
Joshua Smith <joshuawordsmith@gmail.com>
2014-12-09 08:23:14 -0500

    order changes

--- app/scripts/place-order-view.js
+++ app/scripts/place-order-view.js
@@ -1,10 +1,17 @@
 var PlaceOrder = Parse.View.extend ({
 
 	events: {
-
+		'click .customer-state' : 'getCustomer',
+		'click .states-bread' : 'showStates',
+		'click .customers-bread' : 'showCustomers',
+		'click .merchant' : 'showCustomer',
+		'click .accept-merchant' : 'acceptMerchant',
+		'click .cancel-merchant' : 'showStates',
 	},
 
 	template: _.template($('.place-order-view').text()),
+	customersTemplate: _.template($('.customer-by-state-view').text()),
+	customerTemplate: _.template($('.customer-order-view').text()),
 
 	initialize: function() {
 		$('.app-container').html(this.el);
@@ -14,8 +21,66 @@ var PlaceOrder = Parse.View.extend ({
 
 	render: function() {
 		$(this.el).append(this.template());
+		this.showStates();
 
 	},
+
+	showStates: function() {
+		$('.select-customer').html('<span>states</span>');
+
+		var states = [
+			'AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA',
+			'HI','ID','IL','IN','IA','KS','KY','LA','ME','MD',
+			'MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ',
+			'NM','NY','NC','ND','OH','OK','OR','PA','RI','SC',
+			'SD','TN','TX','UT','VT','VA','WA','WV','WI','WY',
+			'DC'
+		];
+
+		states.forEach(function(state){
+			$('.select-customer').append('<div class="customer-state">' + state + '</div>');
+		})
+	},
+
+	getCustomer: function(e) {
+		var that = this;
+		var state = e.currentTarget.innerHTML;
+		this.merchants = [];
+
+		var query = new Parse.Query('customer');
+		query.equalTo('State', state)
+		query.find({
+			success: function(results){
+				results.forEach(function(result){
+					that.merchants.push(result)
+				})
+				// console.log(merchants);
+				that.showCustomers();
+			},
+			error: function(error){
+
+			}
+		})
+	},
+
+	showCustomers: function() {
+		var that = this;
+		$('.select-customer').html('<span><span class="states-bread">states</span> <i class="fa fa-arrow-right"></i> customers</span>');
+		this.merchants.forEach(function(merchant,index){
+			$('.select-customer').append(that.customersTemplate({merchant: merchant, index: index}));
+		});
+	},
+
+	showCustomer: function(location) {
+		$('.select-customer').html('<span><span class="states-bread">states</span> <i class="fa fa-arrow-right"></i> <span class="customers-bread">customers</span> <i class="fa fa-arrow-right"></i> '+ location.currentTarget.innerHTML + '</span>');
+		var index = location.currentTarget.id;
+		this.merchant = this.merchants[index];
+		$('.select-customer').append(this.customerTemplate({merchant : this.merchant}));
+	},
+
+	acceptMerchant: function() {
+		console.log(this.merchant)
+	}
 
 
 });
